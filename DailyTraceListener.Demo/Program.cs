8906e9115efc896d398a1bd557604432b4441a89
Luke Vo <datvm@outlook.com>
2018-10-20 22:57:27 +0700

    Initial Version. Added Demo project.

---
+++ DailyTraceListener.Demo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace DailyTraceListener.Demo
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var traceListener = new DailyTraceListener("Logs")
+                .UseHeader()
+                .UseUtc();
+            Trace.Listeners.Add(traceListener);
+
+            Trace.TraceInformation("Hello");
+            Trace.TraceWarning("Some warning");
+            Trace.TraceError(new Exception("A complete \"error\" message with\r\nmultiple lines.").ToString());
+
+            Trace.WriteLine("Something...");
+        }
+
+    }
+}
--- DailyTraceListener/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace DailyTraceListener
-{
-    public class Class1
-    {
-    }
-}
